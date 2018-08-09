58069ba42859193e7c48eb3d0b2f99de0147a87c
Taylor Gentry <taylor.gentry300@gmail.com>
2018-08-09 10:19:22 -0400

    finished part 2

--- src/App.js
+++ src/App.js
@@ -1,61 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import todoList from './todos.json';
-
-
-class TodoItem extends Component {
-  state = {
-    checked: false,
-    destroy: false
-    }
-
-  handleCheckClick = () =>{
-    this.setState({
-      checked: !this.state.checked
-    })
-  }
-
-  handleDestroyOnButtonClick = () => {
-    this.setState({
-      destroy: !this.state.destroy
-    })
-  }
-
-  render() { 
-
-    return ( 
-        <li className={this.state.checked ? 'completed' : null}>
-          <div className={this.state.destroy ? 'destroy-item' : 'view'}>
-            <input className='toggle' type='checkbox' checked={this.state.checked} 
-            onChange={this.handleCheckClick}/>
-            <label>{this.props.text}</label>
-            <button className='destroy' onClick={this.handleDestroyOnButtonClick}></button>
-          </div>
-        </li>
-     );
-  }
-}
-
-class TodoList extends Component {
-  state = { 
-    todos: todoList
-   }
-  render() { 
-    return ( 
-      <div className='todoapp'>
-        <header className='header'>
-          <h1>todos</h1>
-          <input className='new-todo' placeholder='What needs to be done' autoFocus/>
-        </header>
-        <section className='main'>
-          <ul className='todo-list'>
-            {this.state.todos.map(todo => <TodoItem text={todo.title} key={todo.id}/>)}
-          </ul>
-        </section>
-      </div>
-     );
-  }
-}
+import TodoList from './TodoList'
 
 
 class App extends Component {
